2903b3cdb328a0a5a8ba88473f888dc14b6059af
Dhavaldev01 <dhavalpatel97230@gmail.com>
2024-10-17 16:42:48 +0530

    Change Some CSS

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -22,13 +22,13 @@ function Auth() {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
       <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
-        <div className="flex flex-col gap-10 items-center justify-center">
+        <div className="flex flex-col gap-10 items-center justify-center p-8">
           <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center mb-10">
               <h1 className="text-5xl font-bold md:text-6xl">
                 Welcome
               </h1>
-              <img src={Victory} alt="Vicotry Emoji" className="h-[100px] w-full object-contain" />
+              <img src={Victory} alt="Vicotry Emoji" className=" p-3 h-[100px] w-full object-contain" />
             </div>
             <p className="font-medium text-center">
               Fill in the details to get started with the best chat app!
